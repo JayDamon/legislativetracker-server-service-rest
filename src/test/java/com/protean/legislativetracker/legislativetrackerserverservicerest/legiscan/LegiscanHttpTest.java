c92ca1443ecccc4ce8147aef4e6c192a30cb5ded
DESKTOP-MUAA09M\JayDamon <jay.n.damon@gmail.com>
2018-09-01 12:22:17 -0400

    First Test Created

---
+++ src/test/java/com/protean/legislativetracker/legislativetrackerserverservicerest/legiscan/LegiscanHttpTest.java
@@ -0,0 +1,17 @@
+package com.protean.legislativetracker.legislativetrackerserverservicerest.legiscan;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LegiscanHttpTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void createHttpRequest() {
+    }
+}
